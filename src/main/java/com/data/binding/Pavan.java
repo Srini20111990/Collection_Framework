9d5dd5effba754c38d4b32df1910bf493b8fd723
srini <pothulasrinivvas@gmail.com>
2024-12-14 21:36:17 +0530

    new commit

--- src/main/java/com/data/binding/Pavan.java
+++ src/main/java/com/data/binding/Pavan.java
@@ -4,6 +4,8 @@ public class Pavan {
 	public static void main(String[] args) {
 		System.out.println("this is new commits");
 		System.out.println("this my seconed line");
+		System.out.println("this my fifth line");
+		System.out.println("this my six line");
+	}
 	}
 
-}
